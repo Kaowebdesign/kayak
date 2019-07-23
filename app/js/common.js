b2bb8a329d0d5f38b08f1864a07c4b22a21db206
Andrey <assdwqe@gmail.com>
2019-07-23 17:46:49 +0300

    upd

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    let mainSlider =  $("#mainSlider");
+    var mainSlider =  $("#mainSlider"),
+    aboutSlider = $('#aboutSlider');
     mainSlider.owlCarousel({
         items: 1,
         dots: false,
@@ -9,6 +10,14 @@ $(document).ready(function() {
         // autoplayTimeout:5000,
         animateOut: 'fadeOut'
     });
+    aboutSlider.owlCarousel({
+        items: 4,
+        dots: false,
+        nav: true,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:5000
+    });
     $('.arrowBtn_prev').on('click',function(e){
         e.preventDefault();
         mainSlider.trigger('prev.owl.carousel', [300]);
@@ -16,5 +25,13 @@ $(document).ready(function() {
     $('.arrowBtn_next').on('click',function(e){
         e.preventDefault();
         mainSlider.trigger('next.owl.carousel', [300]);
+    });
+    $('.about-left').on('click',function(e){
+        e.preventDefault();
+        aboutSlider.trigger('prev.owl.carousel', [300]);
+    });
+    $('.about-right').on('click',function(e){
+        e.preventDefault();
+        aboutSlider.trigger('next.owl.carousel', [300]);
     });
 });
